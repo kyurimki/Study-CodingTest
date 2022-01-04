d1a7795668280e974535294555a29d90125de74a
kyurimki <starforest.coding@gmail.com>
2022-01-04 23:08:39 +0900

    Solved : BOJ 1931

---
+++ BOJ/09-Silver2/1931-[그리디]회의실배정/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int [][] timetable = new int[N][2];
+        for(int i = 0; i < N; i++) {
+            timetable[i][0] = sc.nextInt();
+            timetable[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(timetable, (p1, p2) -> p1[0] - p2[0]);
+        Arrays.sort(timetable, (p1, p2) -> p1[1] - p2[1]);
+
+        int time = 0;
+        int answer = 0;
+        for(int i = 0; i < N; i++) {
+            if(timetable[i][0] >= time) {
+                answer++;
+                time = timetable[i][1];
+            }
+        }
+        System.out.println(answer);
+
+    }
+}
