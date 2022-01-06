a20a866a5dc1f079362a686459bf47a54dcd2359
kyurimki <starforest.coding@gmail.com>
2022-01-06 21:19:55 +0900

    Solved : BOJ 2156 | Modified : 1931

--- BOJ/09-Silver2/1931-[그리디]회의실배정/Main.java
+++ BOJ/09-Silver2/1931-[그리디]회의실배정/Main.java
@@ -23,6 +23,6 @@ public class Main {
             }
         }
         System.out.println(answer);
-
+        sc.close();
     }
 }
---
+++ BOJ/10-Silver1/2156-[DP]포도주시식/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int arr[] = new int[N];
+        int dp[] = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if(N >= 1) dp[0] = arr[0];
+        if(N >= 2) dp[1] = arr[0] + arr[1];
+        if(N >= 3) {
+            dp[2] = Math.max(dp[1], Math.max(dp[0]+arr[2], arr[1]+arr[2]));
+            for(int j = 3; j < N; j++) {
+                dp[j] = Math.max(dp[j-1], Math.max(dp[j-2]+arr[j], dp[j-3]+arr[j-1]+arr[j]));
+            }
+        }
+        System.out.println(dp[N-1]);
+        sc.close();
+    }
+}
