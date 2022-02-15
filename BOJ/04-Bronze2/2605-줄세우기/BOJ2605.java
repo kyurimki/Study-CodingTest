44f95a3103759a3b9dc7a7b6a0a5edd2c65be320
kyurimki <starforest.coding@gmail.com>
2022-02-15 17:55:40 +0900

    comment : add comments on BOJ 2605

--- BOJ/04-Bronze2/2605-줄세우기/BOJ2605.java
+++ BOJ/04-Bronze2/2605-줄세우기/BOJ2605.java
@@ -12,8 +12,8 @@ public class BOJ2605 {
         ArrayList<Integer> student = new ArrayList<>();
         for(int i = 1; i <= n; i++) {
             int x = Integer.parseInt(st.nextToken());
-            if(x == 0) student.add(i); // 0일 때는 
-            else student.add(i-x-1, i);
+            if(x == 0) student.add(i); // 0일 때는 원래 자리
+            else student.add(i-x-1, i); // 이외라면 원래 자리에서 뽑은 수만큼 앞으로 이동
         }
         for(int s : student)
             sb.append(s + " ");
