7d945b1e0e7857f0885b49c2ba25312f8760eddb
kyurimki <kyurimki@gmail.com>
2023-02-25 23:35:53 +0900

    Solved : BOJ 17836

--- BOJ/11-Gold5/17836-[BFS]공주님을구해라/BOJ17836.java
+++ BOJ/11-Gold5/17836-[BFS]공주님을구해라/BOJ17836.java
@@ -4,8 +4,9 @@ import java.awt.Point;
 
 public class BOJ17836 {
     static int N, M;
-    static int[][] map;
-
+    static Point gram;
+    static boolean[][] map;
+    static int[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
@@ -17,51 +18,58 @@ public class BOJ17836 {
         M = Integer.parseInt(st.nextToken());
         int T = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        map = new boolean[N][M];
+        visited = new int[N][M];
+        initVisited();
+
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++)
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < M; j++) {
+                int value = Integer.parseInt(st.nextToken());
+                if(value == 2) {
+                    gram = new Point(i, j);
+                    map[i][j] = true;
+                } else if(value == 0) map[i][j] = true;
+            }
         }
 
-        int time = rescue();
+        int time = Integer.min(Integer.MAX_VALUE, move());
+        if(visited[gram.x][gram.y] != -1) {
+            int shortcut = visited[gram.x][gram.y] + (N-1-gram.x) + (M-1-gram.y);
+            time = Integer.min(time, shortcut);
+        }
+
         System.out.println(time > T ? "Fail" : time);
     }
 
-    private static int rescue() {
-        int time = Integer.MAX_VALUE;
+    static int[][] initVisited() {
+        for(int i = 0; i < N; i++) Arrays.fill(visited[i], -1);
+        visited[0][0] = 0;
+        return visited;
+    }
 
-        int[][] visited = new int[N][M];
-        visited[0][0] = 1;
-
+    static int move() {
         Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(0, 0));
+        q.add(new Point(0, 0));
 
         while(!q.isEmpty()) {
             Point p = q.poll();
-
             for(int i = 0; i < 4; i++) {
-                int cx = p.x + dx[i];
-                int cy = p.y + dy[i];
+                int x = p.x + dx[i];
+                int y = p.y + dy[i];
 
-                if(isIn(cx, cy) && (visited[cx][cy] == 0 || visited[cx][cy] > visited[p.x][p.y]+1)) {
-                    if(map[cx][cy] == 0) {
-                        visited[cx][cy] = visited[p.x][p.y] + 1;
-                        q.offer(new Point(cx, cy));
-                    } else if(map[cx][cy] == 2) {
-                        visited[cx][cy] = visited[p.x][p.y] + 1;
-                        time = visited[cx][cy] + Math.abs(N-1-cx) + Math.abs(M-1-cy)-1;
-                    }
+                if(!isInMap(x, y) || !map[x][y]) continue;
+
+                if(visited[x][y] == -1) {
+                    visited[x][y] = visited[p.x][p.y] + 1;
+                    q.add(new Point(x, y));
                 }
             }
         }
-        System.out.println(map[N-1][M-1]);
-        time = map[N-1][M-1] != 0 ? Math.min(time, map[N-1][M-1]-1) : time;
-
-        return time;
+        return visited[N-1][M-1] == -1 ? Integer.MAX_VALUE : visited[N-1][M-1];
     }
 
-    private static boolean isIn(int x, int y) {
-        return x >= 0 && y >= 0 && x < N && y < M;
+    static boolean isInMap(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < M;
     }
 }
